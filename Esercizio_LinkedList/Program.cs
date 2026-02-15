32a745532faed05bb0e08f12f140b9068fd93cae
Alexaxell <nikolovalex08@gmail.com>
2026-02-15 22:57:04 +0100

    Add project files.

---
+++ Esercizio_LinkedList/Program.cs
@@ -0,0 +1,76 @@
+﻿namespace Esercizio_LinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = new Random();
+
+            var head = new LinkedList<int>() { Value = 0 };
+            var current = head;
+
+            for (int i = 0; i < 20; i++)
+            {
+                current.Next = new LinkedList<int> { Value = r.Next(100) };
+                current = current.Next;
+            }
+
+            current = head;
+
+            while (current != null)
+            {
+                Console.WriteLine(current.Value);
+                current = current.Next;
+            }
+
+            Console.WriteLine("Inserisci la posizione di dove vuoi inserire l'elemento: ");
+            int posizione = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Inserisci il valore:");
+            int valore = int.Parse(Console.ReadLine());
+
+            var nuovoNodo = new LinkedList<int> { Value = valore };
+
+            if (posizione == 0)
+            {
+                nuovoNodo.Next = head;
+                head = nuovoNodo;
+            }
+            else
+            {
+                current = head;
+                int index = 0;
+
+                while (current != null && index < posizione - 1)
+                {
+                    current = current.Next;
+                    index++;
+                }
+
+                if (current == null)
+                {
+                    Console.WriteLine("Posizione non valida.");
+                }
+                else
+                {
+                    nuovoNodo.Next = current.Next;
+                    current.Next = nuovoNodo;
+                }
+
+                Console.WriteLine("Lista aggiornata:");
+                current = head;
+                while (current != null)
+                {
+                    Console.WriteLine(current.Value);
+                    current = current.Next;
+                }
+            }
+        }
+    }
+
+    class LinkedList<T>
+    {
+        public T Value { get; set; }
+        public LinkedList<T> Next { get; set; }
+    }
+}
